97f875e2958058b939a6f766dfd1c6676cf8d5b4
Narcissus <1169714441@qq.com>
2019-05-26 15:22:54 +0800

    refactor FizzBuzz.fizzBuzz()

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -1,22 +1,29 @@
 package com.thoughtworks.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FizzBuzz {
+    final private Map<Integer, String> fizzMap;
+
+    public FizzBuzz() {
+        this.fizzMap = new HashMap<>();
+        this.fizzMap.put(3, "Fizz");
+        this.fizzMap.put(5, "Buzz");
+        this.fizzMap.put(7, "Whizz");
+    }
 
     public String fizzBuzz(int i) {
-        String result = "";
-        result += getMappingString(i, 3, "Fizz");
-        result += getMappingString(i, 5, "Buzz");
-        result += getMappingString(i, 7, "Whizz");
-        if(result.equals("")){
+        final String[] result = {""};
+        this.fizzMap.forEach((k, v) -> {
+            if (i % k == 0) {
+                result[0] += v;
+            }
+        });
+        if (result[0].equals("")) {
             return String.valueOf(i);
         }
-        return result;
+        return result[0];
     }
 
-    private String getMappingString(int i, int i2, String fizz) {
-        if (i % i2 == 0) {
-            return fizz;
-        }
-        return "";
-    }
 }
