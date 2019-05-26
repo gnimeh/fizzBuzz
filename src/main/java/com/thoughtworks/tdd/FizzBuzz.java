7eaeb12c2eb70bc87d9c2d0c20c08ca35578b8f0
nn <npr_0815@163.com>
2019-05-26 15:03:13 +0800

    refactor FizzBuzz

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -1,20 +1,22 @@
 package com.thoughtworks.tdd;
 
 public class FizzBuzz {
+
     public String fizzBuzz(int i) {
         String result = "";
-        if (i % 3 == 0) {
-            result += "Fizz";
-        }
-        if (i % 5 == 0) {
-            result += "Buzz";
-        }
-        if (i % 7 == 0) {
-            result += "Whizz";
-        }
+        result += getMappingString(i, 3, "Fizz");
+        result += getMappingString(i, 5, "Buzz");
+        result += getMappingString(i, 7, "Whizz");
         if(result.equals("")){
             return String.valueOf(i);
         }
         return result;
+    }
+
+    private String getMappingString(int i, int i2, String fizz) {
+        if (i % i2 == 0) {
+            return fizz;
+        }
+        return "";
     }
 }
